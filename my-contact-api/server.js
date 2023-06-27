94ac4095e48a31109105e282051583a84f7cac1f
Anshul Yadav <anshul.yadav@w3villa.com>
2023-06-27 15:06:28 +0530

    user authentication done

--- my-contact-api/server.js
+++ my-contact-api/server.js
@@ -2,19 +2,24 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const port = process.env.PORT || 5000;
 
-const contactRoutes = require("./routes/contactRoutes");
-const userRoutes = require("./routes/userRoutes");
-const errorHandler = require("./middleWare/errorHandler");
-
+//database connection
 const conncetDB = require("./config/DBConnection");
 conncetDB();
+
+//error handler middleware
+const errorHandler = require("./middleWare/errorHandler");
+
+//routes
+const contactRoutes = require("./routes/contactRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 app.use(express.json());
 app.use("/api/contacts", contactRoutes);
 app.use("/api/user", userRoutes);
 app.use(errorHandler);
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, (err) => {
     if (err) {
