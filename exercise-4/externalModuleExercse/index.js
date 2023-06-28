dbb040dd19884714fa9580ab15eba1854663d745
Anshul Yadav <anshul.yadav@w3villa.com>
2023-06-28 12:10:48 +0530

    refctored code

--- exercise-4/externalModuleExercse/index.js
+++ exercise-4/externalModuleExercse/index.js
@@ -1,21 +1,25 @@
-const http = require('http'); //core module
-const {getFactorial} = require("../factorialExercise/factorialModule"); //local module
+const http = require("http"); //core module
+const { getFactorial } = require("../factorialExercise/factorialModule"); //local module
 const greet = require("./greetModule"); //local module
 const R = require("ramda"); // npm package module
- 
-http.createServer((request,response)=>{
-    response.writeHead(200,{"Content-Type":"text/html"});
+
+http.createServer((request, response) => {
+    response.writeHead(200, { "Content-Type": "text/html" });
 
     console.log("server started");
 
     response.write(greet());
+    console.log(greet());
     response.write(`<br>factorial of 50 is : ${getFactorial(50)}`);
+    console.log(`factorial of 50 is : ${getFactorial(50)}`);
 
-    const age = [20,30,10];
-    const forEachBlock =(age)=> {
+    const age = [20, 30, 10];
+    const forEachBlock = (age) => {
         response.write(`<br> Age is : ${age}  `);
-    }
-    R.forEach(forEachBlock,age);
+        console.log(` Age is : ${age}  `);
+    };
+    R.forEach(forEachBlock, age);
 
     response.end();
 }).listen("5000");
+console.log(`Server started at http://localhost:5000/`);
